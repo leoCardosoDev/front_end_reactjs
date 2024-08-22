6dab00a279d7b51c33ca9dd1da5afc35f7024848
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-22 14:51:48 -0700

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { GitHubBanner, Refine } from "@refinedev/core";
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -7,15 +7,15 @@ import {
 } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import {dataProvider, liveProvider } from './providers'
+import { authProvider, dataProvider, liveProvider } from './providers'
+import { HomePage, ForgotPassword, Login, Register} from './pages'
 
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
-import { BrowserRouter, Routes } from "react-router-dom";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -29,7 +29,7 @@ function App() {
                 liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
-                // authProvider={}
+                authProvider={authProvider}
                 resources={[
                 ]}
                 options={{
@@ -40,8 +40,12 @@ function App() {
                   liveMode: "auto",
                 }}
               >
-                <Routes>
-                </Routes>
+              <Routes>
+                <Route index element={<HomePage />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+              </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
                 <DocumentTitleHandler />
@@ -49,7 +53,7 @@ function App() {
               <DevtoolsPanel />
             </DevtoolsProvider>
           </AntdApp>
-      
+
       </RefineKbarProvider>
     </BrowserRouter>
   );
